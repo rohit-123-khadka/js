63f35ebe783ed32c77dad59a4514a60824c5334c
rohit-123-khadka <khadkarohit2006@gmail.com>
2026-04-28 15:26:42 +0545

    about \n new extra line

--- Strings.js/backtick.js
+++ Strings.js/backtick.js
@@ -1,13 +1,17 @@
-// Write a JavaScript program using backticks to store your name and age, then display them using console.log().
+// 1. Write a JavaScript program using backticks to store your name and age, then display them using console.log().
 
 // let name = "Rohit";
 // let age = 20;
 
 // console.log(`My name is ${name} and I am ${age} years old.`);
 
-// Write a JavaScript program using backticks to store a product name and price, then display them using console.log().
+// 2. Write a JavaScript program using backticks to store a product name and price, then display them using console.log().
 
 // let product = "Shoes";
 // let price = 50;
 
 // console.log(`The product is ${product} and the price is $${price}.`);
+
+// 3. use a multi-line String, create the text like Total cost: $8.94 and add a line of text underneath: 'Thank you, come again!' display both line as a popup.
+
+// alert(`Total cost: $${(500+300+100)/100}\n Thank you, come again!`)
